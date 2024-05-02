6340b8a4cff4352a07243ac808c1220a8496385a
matt <matt@localhost.localdomain>
2024-05-02 18:12:07 +0100

    Working Studio Ghibli API GET call example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StudioGhibliComponent } from './studio-ghibli/studio-ghibli.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'studio-ghibli',
+    component: StudioGhibliComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  },
+];
